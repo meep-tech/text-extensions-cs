ebfec061e45d36777de92a28aaac60b6c5f0ee57
meep.tech <meepdottech@gmail.com>
2024-05-26 03:15:08 -0400

    Initial Commit, chars and strings

---
+++ src/Chars.cs
@@ -0,0 +1,15 @@
+﻿namespace Meep.Tech.Text {
+    public static class CharExtensions {
+        public static bool IsWhiteSpace(this char c) 
+            => char.IsWhiteSpace(c);
+
+        public static bool IsLetter(this char c)
+            => char.IsLetter(c);
+
+        public static bool IsDigit(this char c)
+            => char.IsDigit(c);
+
+        public static bool IsLetterOrDigit(this char c)
+            => char.IsLetterOrDigit(c);
+    }
+}
---
+++ src/Strings.cs
@@ -0,0 +1,15 @@
+﻿namespace Meep.Tech.Text {
+    public static class StringExtensions {
+        public static bool IsNullOrWhiteSpace(this string? s)
+            => string.IsNullOrWhiteSpace(s);
+
+        public static bool IsNullOrEmpty(this string? s)
+            => string.IsNullOrEmpty(s);
+
+        public static bool IsNotNullOrEmpty(this string? s)
+            => !string.IsNullOrEmpty(s);
+
+        public static bool IsNotNullOrWhiteSpace(this string? s)
+            => !string.IsNullOrWhiteSpace(s);
+    }
+}
